12e4b2e752f52acc51e5b5f60cd60c92048bd284
Kenneth Weber <kenneth.weber1@ontariotechu.net>
2023-04-04 01:50:22 -0400

    first commit

---
+++ core.py
@@ -0,0 +1,31 @@
+import ctypes
+import os
+from sys import platform, version
+
+
+class Core:
+    def __init__(self):
+        '''
+
+        A setup program that does some python-os interfacing, for a more stable demo.
+
+        '''
+        from sys import platform
+        print("CORE OPENED")
+        if platform == "linux" or platform == "linux2":
+            # TO-DO linux
+            print("Please try this demo from a windows device")
+            exit()
+        elif platform == "darwin":
+            # TO-DO darwin
+            print("Please try this demo from a windows device")
+            exit()
+        elif platform == "win32":
+            self.user32 = ctypes.windll.user32
+            self.screensize = self.user32.GetSystemMetrics(78), self.user32.GetSystemMetrics(79)
+            self.version = version[0:4].rstrip(".")
+            print("os.system calls beginning")
+            os.system('cmd /c "pip install --upgrade pip"')
+            os.system('cmd /c "pip install pip-review"')
+            os.system('cmd /c "pip-review --auto"')
+            print("Your system's pip installer version and every dependancy of this software has been checked for updates automatically.")
---
+++ main.py
@@ -0,0 +1,9 @@
+from core import Core
+
+if __name__ == "__main__":
+    """
+
+    Written in C++ style, the main.py script is the sole callable file needed to run everything.
+
+    """
+    vision_demo = Core()
